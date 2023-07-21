63f35ab14af28bc0bda63e21bea9106b52c7cf2b
Vin529 <kennyvinnie@gmail.com>
2023-07-21 19:35:20 +0100

    initial work

---
+++ indexScript.js
@@ -0,0 +1,10 @@
+document.getElementById("exerciseSelectButton").onclick = display_exercises;
+
+function display_exercises () {
+    exerciseArray = get_exercises();
+    document.getElementById("")
+}
+
+function get_exercises () {
+    return ["bench", "squat", "pullup"];
+}
---
+++ workout entry.py
@@ -0,0 +1,11 @@
+workoutObject = {}
+workoutObject["ID"] = ""
+workoutObject["Date"] == ""
+workoutObject["Duration"] = ""
+
+
+exerciseList = []
+exerciseObject = {}
+exerciseList.append(exerciseObject)
+
+workoutObject["Exercises"] = ExerciseList
